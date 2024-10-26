b1bab4ce67645443df84d488a303adcb84664d31
Максим Самисько <masechk@gmail.com>
2024-10-26 21:38:12 +0300

    Initial commit

---
+++ DeliveryOrders/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryOrders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
