3866c654046160e489dd10103da47d3067f58a59
Максим Самисько <masechk@gmail.com>
2024-10-27 21:52:16 +0300

    Добавлено подключение к бд

---
+++ DeliveryOrders/DbConnection/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryOrders.DbConnection
+{
+    public static class ConnectionString
+    {
+        public static string MsSqlConnection => @"Server=.\SQLEXPRESS;Database=EffectiveMobileDelivery;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
---
+++ DeliveryOrders/DbConnection/Connector.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryOrders.DbConnection
+{
+    internal class Connector
+    {
+        private SqlConnection connection;
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                connection = new SqlConnection(ConnectionString.MsSqlConnection);
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+    }
+}
